720cd59f7a06d73c03625b6e2eb1e73da2d0c774
「ℓαυ」 <54045431+FreshSurround@users.noreply.github.com>
2026-03-25 17:50:43 +0000

    Deleted old backend directory

--- backend/tests/test_main.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from .maintest import app
-
-test_client = TestClient(app)
-
-def test_main():
-    respuesta = test_client.get("/")
-    assert respuesta.status_code == 200
-    assert respuesta.json() == {"test": "corriendo correctamente"}
